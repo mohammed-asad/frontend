4810aa6c5574da33353f55f9a28bb698c0fc416c
asad <asad.ipcs@gmail.com>
2020-02-13 20:02:13 +0530

    home navigation changes till mobile

--- application/views/pages/sections/navigation.php
+++ application/views/pages/sections/navigation.php
@@ -1,147 +1,148 @@
 <div class="nav-wrapper">
-<section class="navigation <?php echo $home; ?>">
-	<p class="right-toptext">Your Dream Destination is just Call Away :<span> +91 9845 944499</span></p>
-	<div class="position">
-		<div class="blur-backdrop"></div>
-		<div class="hamburger-menu"></div>
-	</div>
-	<a href="<?php echo base_url(); ?>" class="logo mobile-logo page-logo">
-		<img class="img-fluid  <?php echo $home ?>" src="<?php echo base_url(); ?>assets/images/logo/gray-logo.svg" alt="">
-		<img class="img-fluid <?php echo $home ?>1" src="<?php echo base_url(); ?>assets/images/logo/logo.svg" alt="">
-	</a>
+	<section class="navigation <?php echo $home; ?>">
+		<p class="right-toptext">Your Dream Destination is just Call Away :<span> +91 9845 944499</span></p>
+		<div class="position">
+			<div class="blur-backdrop"></div>
+			<div class="hamburger-menu change-hamburger-menu"></div>
+		</div>
+		<a href="<?php echo base_url(); ?>" class="logo mobile-logo page-logo">
+			<img class="img-fluid  <?php echo $home ?>" src="<?php echo base_url(); ?>assets/images/logo/gray-logo.svg" alt="">
+			<img class="img-fluid <?php echo $home ?>1" src="<?php echo base_url(); ?>assets/images/logo/logo.svg" alt="">
+		</a>
 
-	<div class="nav-items-holder">
-		<span class="close-hamburger-menu"></span>
-		<a href="<?php echo base_url(); ?>" class="logo secondary">
-			<img src="<?php echo base_url(); ?>assets/images/logo/gray-logo.svg" alt="">
-		</a>
-		<a href="<?php echo base_url(); ?>travelprivate" class="nav-item">
-			<span>
-				Travel private
-			</span>
-		</a>
-		<div class="nav-item" data-bluritem="true">
-			<span>Destinations</span>
-			<div class="sub-menu">
-				<!-- Sub Menu Start-->
-				<div class="sub-menu-items-holder">
-					<span class="reset-menu"></span>
-					<!-- Sub Menu Region Start-->
-					<div class="sub-menu-item-list">
-						<div class="sub-menu-item active">
-							<span data-region="europe">
-								Europe
-							</span>
-							<div class="sub-item-desc" data-region="europe">
+		<div class="nav-items-holder">
+			<span class="close-hamburger-menu"></span>
+			<p class="right-toptext1" style="color:#fff;">Your Dream Destination is just Call Away :<span> +91 9845 944499</span></p>
+			<a href="<?php echo base_url(); ?>" class="logo secondary">
+				<img src="<?php echo base_url(); ?>assets/images/logo/gray-logo.svg" alt="">
+			</a>
+			<a href="<?php echo base_url(); ?>travelprivate" class="nav-item">
+				<span>
+					Travel private
+				</span>
+			</a>
+			<div class="nav-item" data-bluritem="true">
+				<span>Destinations</span>
+				<div class="sub-menu">
+					<!-- Sub Menu Start-->
+					<div class="sub-menu-items-holder">
+						<span class="reset-menu"></span>
+						<!-- Sub Menu Region Start-->
+						<div class="sub-menu-item-list">
+							<div class="sub-menu-item active">
+								<span data-region="europe">
+									Europe
+								</span>
+								<div class="sub-item-desc" data-region="europe">
+									<div class="sub-item-item overview">
+										<h4>Overview</h4>
+										<p>Europe is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere.
+											It is bordered by the Arctic Ocean to the north,
+											the Atlantic Ocean to the west, Asia to the east, and the Mediterranean Sea to the south.
+											It comprises the westernmost part of Eurasia.
+										</p>
+										<a href="#">View all <span class="regionnames"></span></a>
+									</div>
+									<div class="sub-item-item countries">
+										<h4>Destinations</h4>
+										<ul>
+											<li>Switzerland</li>
+											<li>France</li>
+											<li>Turkey</li>
+										</ul>
+									</div>
+									<div class="sub-item-item slider"></div>
+								</div>
+							</div>
+
+							<div class="sub-menu-item">
+								<span data-region="asia">
+									Asia
+								</span>
+								<div class="sub-item-desc" data-region="europe">
+									<div class="sub-item-item overview">
+										<h4>Overview</h4>
+										<p>Asia is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere.
+											It is bordered by the Arctic Ocean to the north,
+											the Atlantic Ocean to the west, Asia to the east, and the Mediterranean Sea to the south.
+											It comprises the westernmost part of Eurasia.
+										</p>
+									</div>
+									<div class="sub-item-item countries">
+										<h4>Destinations</h4>
+										<ul>
+											<li>Switzerland</li>
+											<li>Turkey</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="sub-menu-item">
+								<span data-region="america">
+									America
+								</span>
+								<div class="sub-item-desc" data-region="europe">
+									<div class="sub-item-item overview">
+										<h4>Overview</h4>
+										<p>America is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere.
+											It is bordered by the Arctic Ocean to the north,
+											the Atlantic Ocean to the west, Asia to the east, and the Mediterranean Sea to the south.
+											It comprises the westernmost part of Eurasia.
+										</p>
+									</div>
+									<div class="sub-item-item countries">
+										<h4>Destinations</h4>
+										<ul>
+											<li>France</li>
+											<li>Italy</li>
+											<li>Turkey</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+						</div>
+						<!-- Sub Menu Region Ends-->
+
+						<!-- Sub Menu Detauls Start-->
+						<div class="sub-item-desc-holder">
+
+
+							<div class="sub-item-desc" data-region="asia">
 								<div class="sub-item-item overview">
-									<h4>Overview</h4>
+									<h4 class="mb-2">Overview</h4>
 									<p>Europe is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere.
 										It is bordered by the Arctic Ocean to the north,
 										the Atlantic Ocean to the west, Asia to the east, and the Mediterranean Sea to the south.
-										It comprises the westernmost part of Eurasia.
-									</p>
-									<a href="#">View all <span class="regionnames"></span></a>
+										It comprises the westernmost part of Eurasia.</p>
 								</div>
 								<div class="sub-item-item countries">
-									<h4>Destinations</h4>
+									<h4 class="mb-2">Destinations</h4>
 									<ul>
-										<li>Switzerland</li>
-										<li>France</li>
-										<li>Turkey</li>
-									</ul>
-								</div>
-								<div class="sub-item-item slider"></div>
-							</div>
-						</div>
-
-						<div class="sub-menu-item">
-							<span data-region="asia">
-								Asia
-							</span>
-							<div class="sub-item-desc" data-region="europe">
-								<div class="sub-item-item overview">
-									<h4>Overview</h4>
-									<p>Asia is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere.
-										It is bordered by the Arctic Ocean to the north,
-										the Atlantic Ocean to the west, Asia to the east, and the Mediterranean Sea to the south.
-										It comprises the westernmost part of Eurasia.
-									</p>
-								</div>
-								<div class="sub-item-item countries">
-									<h4>Destinations</h4>
-									<ul>
-										<li>Switzerland</li>
-										<li>Turkey</li>
+										<li>India</li>
+										<li>Indonesia</li>
+										<li>Bangladesh</li>
+										<li>Pakistan</li>
 									</ul>
 								</div>
 							</div>
-						</div>
 
-						<div class="sub-menu-item">
-							<span data-region="america">
-								America
-							</span>
-							<div class="sub-item-desc" data-region="europe">
-								<div class="sub-item-item overview">
-									<h4>Overview</h4>
-									<p>America is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere.
-										It is bordered by the Arctic Ocean to the north,
-										the Atlantic Ocean to the west, Asia to the east, and the Mediterranean Sea to the south.
-										It comprises the westernmost part of Eurasia.
-									</p>
-								</div>
-								<div class="sub-item-item countries">
-									<h4>Destinations</h4>
-									<ul>
-										<li>France</li>
-										<li>Italy</li>
-										<li>Turkey</li>
-									</ul>
-								</div>
-							</div>
 						</div>
-
+						<!-- Sub Menu Detauls End-->
 					</div>
-					<!-- Sub Menu Region Ends-->
-
-					<!-- Sub Menu Detauls Start-->
-					<div class="sub-item-desc-holder">
-
-
-						<div class="sub-item-desc" data-region="asia">
-							<div class="sub-item-item overview">
-								<h4 class="mb-2">Overview</h4>
-								<p>Europe is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere.
-									It is bordered by the Arctic Ocean to the north,
-									the Atlantic Ocean to the west, Asia to the east, and the Mediterranean Sea to the south.
-									It comprises the westernmost part of Eurasia.</p>
-							</div>
-							<div class="sub-item-item countries">
-								<h4 class="mb-2">Destinations</h4>
-								<ul>
-									<li>India</li>
-									<li>Indonesia</li>
-									<li>Bangladesh</li>
-									<li>Pakistan</li>
-								</ul>
-							</div>
-						</div>
-
-					</div>
-					<!-- Sub Menu Detauls End-->
+					<!-- Sub Menu End-->
 				</div>
-				<!-- Sub Menu End-->
 			</div>
+			<a href="<?php echo base_url(); ?>ourspecialities" class="nav-item">
+				<span>
+					Our Specialities
+				</span>
+			</a>
 		</div>
-		<a href="<?php echo base_url(); ?>ourspecialities" class="nav-item">
-			<span>
-				Our Specialities
-			</span>
-		</a>
-	</div>
-	<div class="nav-texts">
+		<div class="nav-texts">
 
-	</div>
+		</div>
 
-</section>
+	</section>
 </div>
